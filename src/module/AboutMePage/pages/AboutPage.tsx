091e7c5af97e0711922ba08b93eefd316d626bf0
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-14 18:37:44 +0700

    add desain card for about me

--- src/module/AboutMePage/pages/AboutPage.tsx
+++ src/module/AboutMePage/pages/AboutPage.tsx
@@ -1,8 +1,31 @@
 import type React from "react"
+import '../styles/index.css'
+import { CAvatar, CCard, CCardBody, CCol, CContainer, CImage, CRow } from "@coreui/react";
+import profileImage from '../../../assets/ujang.jpg'
 
-const AboutPage:React.FC  = () => {
+const AboutPage: React.FC = () => {
   return (
-    <div className="text-white">AboutPage</div>
+    <section className='layout-section'>
+      <CContainer className="layout-container">
+        <CCard className="card-index">
+          <CCardBody className="card-body">
+            <CRow className="align-items-center">
+              <CCol md={6} className="text-center d-none d-md-block">
+                <div>
+                  <CImage rounded src={profileImage}/>
+                </div>
+              </CCol>
+              <CCol md={6}>
+                <h1 className="welcome-title">TENTANG SAYA</h1>
+                <p className="welcome-content">
+                  Saya seorang Frontend Developer dengan pengalaman lebih dari 2 tahun bekerja dalam pengembangan aplikasi web menggunakan framework modern seperti Vue.js, React.js dan Angular. Terampil dalam mengimplementasikan desain dari Figma, memastikan desain yang responsif di semua perangkat. Pengalaman saya mencangkup state management(Redux/Vuex), integrasi RESTful API berkolaborasi dengan tim Backend, serta penerapan unit testing(Vitest/Jasmine). Saya terbiasa bekerja dalam tim menggunakan GIT sebagai version control.
+                </p>
+              </CCol>
+            </CRow>
+          </CCardBody>
+        </CCard>
+      </CContainer>
+    </section>
   )
 }
 
--- src/module/OpeningPage/pages/OpeningPage.tsx
+++ src/module/OpeningPage/pages/OpeningPage.tsx
@@ -12,29 +12,27 @@ const OpeningPage: React.FC = () => {
   };
   return (
     <section className='layout-section'>
-      <section className="layout-section layout-first">
-        <CContainer className="layout-container">
-          <CRow className="align-items-center">
-            <CCol md={6}>
-              <h2 className="welcome-subtitle">Hai, Selamat Datang</h2>
-              <h1 className="welcome-title">UJANG ARIPIN</h1>
-              <p className="welcome-content">
-                Software Developer | Linux Enthusiast
-              </p>
-              <CButton onClick={handleDownload} className="space-btn text-white">
-                🚀 Download PDF
-              </CButton>
-            </CCol>
-            <CCol md={6} className="text-center">
-              <img
-                src={SideImage}
-                alt="Welcome"
-                className="welcome-image"
-              />
-            </CCol>
-          </CRow>
-        </CContainer>
-      </section>
+      <CContainer className="layout-container">
+        <CRow className="align-items-center">
+          <CCol md={6}>
+            <h2 className="welcome-subtitle">Hai, Selamat Datang</h2>
+            <h1 className="welcome-title">UJANG ARIPIN</h1>
+            <p className="welcome-content">
+              Software Developer | Linux Enthusiast
+            </p>
+            <CButton onClick={handleDownload} className="space-btn text-white">
+              🚀 Download PDF
+            </CButton>
+          </CCol>
+          <CCol md={6} className="text-center d-none d-md-block">
+            <img
+              src={SideImage}
+              alt="Welcome"
+              className="welcome-image"
+            />
+          </CCol>
+        </CRow>
+      </CContainer>
     </section>
   )
 }
