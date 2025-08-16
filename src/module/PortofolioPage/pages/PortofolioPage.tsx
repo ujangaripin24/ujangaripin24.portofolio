2fbcf5bd10ee7e152144a7cca66b04e4fa8089be
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-16 14:31:14 +0700

    portofolio success created

--- src/module/PortofolioPage/pages/PortofolioPage.tsx
+++ src/module/PortofolioPage/pages/PortofolioPage.tsx
@@ -6,39 +6,39 @@ import dataPortofolio from '../../../assets/data/data-protofolio.json'
 const PortofolioPage: React.FC = () => {
   const [visible, setVisible] = useState<number | null>(null)
   return (
-      <CContainer>
-        <div className="text-center">
-          <h1 className="portofolio-title">PORTOFOLIO SAYA</h1>
-        </div>
-        <div>
-          <CRow className="g-4">
-            {dataPortofolio.data.map((item) => (
-              <CCol key={item.id} lg={6} md={12}>
-                <CCard style={{ width: '100%' }}>
-                  <CCardImage orientation="top" src={item.image_link} />
-                  <CCardBody>
-                    <CCardTitle>{item.nama_proyek}</CCardTitle>
-                    <CCardText>
-                      {item.teknologi.map((tech) => tech.nama_teknologi).join(', ')}
-                    </CCardText>
-                    <CButton
-                      color="primary"
-                      onClick={() => setVisible(visible === item.id ? null : item.id)}
-                    >
-                      Detail Proyek
-                    </CButton>
-                    <CCollapse visible={visible === item.id}>
-                      <CCard className="mt-3">
-                        <CCardBody>{item.detail_proyek}</CCardBody>
-                      </CCard>
-                    </CCollapse>
-                  </CCardBody>
-                </CCard>
-              </CCol>
-            ))}
-          </CRow>
-        </div>
-      </CContainer>
+    <CContainer>
+      <div className="text-center">
+        <h1 className="portofolio-title">PORTOFOLIO SAYA</h1>
+      </div>
+      <div>
+        <CRow className="g-4">
+          {dataPortofolio.data.map((item) => (
+            <CCol key={item.id} lg={6} md={12}>
+              <CCard style={{ width: '100%' }} className='card'>
+                <CCardImage orientation="top" src={item.image_link} width="100%" height="300px" />
+                <CCardBody className='card-body'>
+                  <CCardTitle>{item.nama_proyek}</CCardTitle>
+                  <CCardText>
+                    {item.teknologi.map((tech) => tech.nama_teknologi).join(', ')}
+                  </CCardText>
+                  <CButton
+                    color="primary"
+                    onClick={() => setVisible(visible === item.id ? null : item.id)}
+                  >
+                    Detail Proyek
+                  </CButton>
+                  <CCollapse visible={visible === item.id}>
+                    <CCard className="mt-3">
+                      <CCardBody><p>{item.detail_proyek}</p></CCardBody>
+                    </CCard>
+                  </CCollapse>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          ))}
+        </CRow>
+      </div>
+    </CContainer>
   )
 }
 
