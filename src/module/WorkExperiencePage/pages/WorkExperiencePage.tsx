395aa0a2b7ea804a63ee60fad4ff03940a7c4d8f
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-16 15:37:57 +0700

    add work expr

--- src/module/WorkExperiencePage/pages/WorkExperiencePage.tsx
+++ src/module/WorkExperiencePage/pages/WorkExperiencePage.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import {
+  CContainer,
+  CRow,
+  CCol,
+  CCard,
+  CCardBody,
+  CCardTitle,
+  CCardText
+} from '@coreui/react'
+import '../styles/index.css'
+import experienceData from '../../../assets/data/data-work-expreince.json'
 
-const WorkExperiencePage:React.FC = () => {
+export const WorkExperiencePage: React.FC = () => {
   return (
-    <div className="text-white">WorkExperiencePage</div>
+    <CContainer className="experience-container">
+      <h1 className="neon-title text-center mb-4">Pengalaman Kerja</h1>
+      <CRow className="g-4 justify-content-center">
+        {experienceData.data.map((exp) => (
+          <CCol key={exp.id} md={6} lg={4}>
+            <CCard className="experience-card h-100">
+              <CCardBody>
+                <CCardTitle><div className="neon-subtitle">{exp.perusahaan}</div></CCardTitle>
+                <h6 className="posisi">{exp.posisi}</h6>
+                <h6 className="tahun">{exp.tahun}</h6>
+                <CCardText className="deskripsi">{exp.deskripsi}</CCardText>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        ))}
+      </CRow>
+    </CContainer>
   )
 }
 
-export default WorkExperiencePage
+export default WorkExperiencePage;
